a4207a2732869c24076d687db89120dc4cda0185
alikmpt8529 <m80693827@gmail.com>
2024-06-24 16:38:48 +0900

    ver 1.3.2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import jsPDF from 'jspdf';
+
 // Todoアイテムのプロパティ型定義
 type TodoItemProps = {
   todo: { text: string; isCompleted: boolean; deadline: string };
